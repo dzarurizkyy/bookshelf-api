57d46940bc03b84091304320bb8d721d7b5ee0a4
dzarurizkyy <dzarurizkybusiness@gmail.com>
2023-09-23 15:55:59 +0700

    Create web server with port 9000

--- src/server.js
+++ src/server.js
@@ -1,1 +1,27 @@
-console.log('Hello World !!')
+/* Web Framework */
+const Hapi = require('@hapi/hapi')
+
+/* Import Module */
+const routes = require('./routes')
+
+/* Configuration Web Server */
+const init = async () => {
+  const server = Hapi.server({
+    port: 9000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      /* Same Origin Policy */
+      cors: {
+        origin: ['*']
+      }
+    }
+  })
+  /* Configuration Routes */
+  // server.route(routes)
+
+  await server.start()
+  console.log(`Server berjalan pada ${server.info.uri}`)
+}
+
+/* Enable Web Server */
+init()
